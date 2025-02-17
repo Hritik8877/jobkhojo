858f59c97d6e3c76f208b752353e265fb1ee041a
Hritik kumar <hritikkyd8877@gmail.com>
2025-02-17 23:35:50 +0530

    establish database connection with MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 dotenv.config({});
 const app=express();
+connectDB();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
