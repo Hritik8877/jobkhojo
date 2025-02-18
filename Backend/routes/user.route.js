0b9df249e90318de736f6883917555cc62d18c56
Hritik kumar <hritikkyd8877@gmail.com>
2025-02-19 00:10:35 +0530

    update logout route to use GET method

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -2,10 +2,10 @@ import express from "express";
 import { login, logout, register, updateprofile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 const userRoute=express.Router();
-
+    
 userRoute.route("/register").post(register);
 userRoute.route("/login").post(login);
-userRoute.route("/logout").post(logout);
+userRoute.route("/logout").get(logout);
 userRoute.route("/profile/update").post(isAuthenticated,updateprofile);
 
 export default userRoute;
