d5936260f7f9aa9616886e695920c22d65c90305
Hritik kumar <hritikkyd8877@gmail.com>
2025-02-23 02:35:41 +0530

    feat: implement routing with React Router and add authentication components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Navbar from './components/shared/Navbar.jsx'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Signup from './components/auth/Signup.jsx';
+import Home from './components/Home.jsx';
+import Login from './components/auth/Login.jsx';
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login', 
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  }
+]);
 
 const App = () => {
   return (
     <>
-    <Navbar/>
+      <RouterProvider router={appRouter} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
