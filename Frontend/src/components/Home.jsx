098b550bf7e81b536b9068549ef3c6fd38ba587a
Hritik kumar <hritikkyd8877@gmail.com>
2025-03-03 23:22:59 +0530

    add CategoryCarouser component to Home; implement carousel for job categories

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
+import CategoryCarouser from '@/CategoryCarouser'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <HeroSection/>
+      <CategoryCarouser/>
     </div>
   )
 }
