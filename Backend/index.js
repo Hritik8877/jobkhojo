a210d1fddc81fde22b336255daa6cbc00bb8e700
Hritik kumar <hritikkyd8877@gmail.com>
2025-06-25 02:21:41 +0530

    fix: correct CORS origin configuration to ensure proper request handling

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsoption={
-    origin: 'https://jobkhojo-f.onrender.com',
+    origin: 'https://jobkhojo-f.onrender.com '|| 'http://localhost:5173/',
     credentials:true,
 
 }
