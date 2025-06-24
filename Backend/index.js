03ed9941daaea4ca1d89652e8bbca9b86c3c9b22
Hritik kumar <hritikkyd8877@gmail.com>
2025-06-25 02:16:39 +0530

    fix: update CORS origin to allow requests from the production URL

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsoption={
-    origin: 'http://localhost:5173',
+    origin: 'https://jobkhojo-f.onrender.com',
     credentials:true,
 
 }
